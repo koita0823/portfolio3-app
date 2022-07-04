6563b175620d7e9c7a54a17868e8ce0708adb4df
koita <koitabashi18@yahoo.co.jp>
2022-07-04 13:25:10 +0900

    first commit

---
+++ resources/views/components/tweet/form/image.blade.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="{{ asset('css/image.css') }}">
+<div class="imageForm" x-data="inputFormHandler()">
+    <template x-for"(field, i) in fields" :key="i">
+        <div class="imageForm2">
+            <label 
+            
+        </div>
+    </template>
+</div>
